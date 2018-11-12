f091195a0443480343f075a9a4edbf11d3183e5c
weixuefeng <weixuefeng@lubangame.com>
2018-11-13 01:38:00 +0800

    complete cartwalletmanager

--- src/app/src/main/java/com/feng/digitacoin/ui/me/adapter/WalletCartLayoutManager.kt
+++ src/app/src/main/java/com/feng/digitacoin/ui/me/adapter/WalletCartLayoutManager.kt
@@ -1,8 +1,9 @@
 package com.feng.digitacoin.ui.me.adapter
 
 import android.animation.ValueAnimator
-import android.content.ContentValues.TAG
 import android.content.Context
+import android.graphics.Rect
+import android.support.v4.util.SparseArrayCompat
 import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.util.SparseArray
@@ -18,36 +19,40 @@ import android.view.ViewGroup
  */
 class WalletCartLayoutManager(private val context: Context?) : RecyclerView.LayoutManager(){
 
-    val MAX_SHOW_COUNT = 4
-
-    val SCALE_GAP = 0.05F
-
-    var mXoffset = 0
+    private var mXoffset = 0
 
     // 所有view移动的宽度范围
-    var mTotalWidth = 0
+    private var mTotalWidth = 0
 
     // 子view测量宽度
-    var childViewMeasureWidth = 0
+    private var childViewMeasureWidth = 0
     // 子view测量宽度
-    var childViewMeasureHeight = 0
+    private var childViewMeasureHeight = 0
 
     // 子view占据宽度 = 子view测量宽度 + 距离下一个view 的间距 childViewWidth + widthSpace
-    var childViewWidth = 0
+    private var childViewWidth = 0
 
     // 开始view距离左右两边的空间
-    var startWidthSpace = 0
+    private var startWidthSpace = 0
 
     // 开始view距离上下的空间
-    var startHeightSpace = 0
+    private var startHeightSpace = 0
 
     // 两个view之间的间距
-    var widthSpace = 0
+    private var widthSpace = 0
 
     // 自动选中的动画时间
-    val mFixingAnimationDuration = 200L
+    private val mFixingAnimationDuration = 200L
 
-    private val TRANS_Y_GAP : Int = (context!!.resources.displayMetrics.density * 20).toInt()
+    // 记录所有view 的偏移信息
+    private val itemFrames = SparseArrayCompat<Rect>()
+
+    // 第一条可见的item
+    private var mFirstVisibleItemPos = 0
+
+    // recycler
+    private var mRecycler : RecyclerView.Recycler? = null
+
 
     override fun generateDefaultLayoutParams(): RecyclerView.LayoutParams {
         return RecyclerView.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
@@ -55,12 +60,12 @@ class WalletCartLayoutManager(private val context: Context?) : RecyclerView.Layo
     }
 
     override fun onLayoutChildren(recycler: RecyclerView.Recycler?, state: RecyclerView.State?) {
+        mRecycler = recycler
         if(state!!.itemCount == 0) {
             removeAndRecycleAllViews(recycler)
             return
         }
         detachAndScrapAttachedViews(recycler)
-
         val childView = recycler!!.getViewForPosition(0)
         measureChildWithMargins(childView, 0, 0)
         // 每个子view的实际宽度
@@ -72,25 +77,20 @@ class WalletCartLayoutManager(private val context: Context?) : RecyclerView.Layo
 
         widthSpace = startWidthSpace / 2
 
-
         childViewWidth = childViewMeasureWidth + widthSpace
         mTotalWidth = (itemCount - 1) * childViewWidth
 
         layoutChildView(recycler, state)
-
     }
 
     private fun layoutChildView(recycler: RecyclerView.Recycler?, state: RecyclerView.State) {
         val needLayoutItems = getNeedLayoutItems(recycler)
         if(needLayoutItems.size() == 0 || state.itemCount == 0) {
-            Log.e("LayoutChildView", "IS NULL")
             removeAndRecycleAllViews(recycler)
             return
         }
-        Log.e("LayoutChildView", "needLayoutItems:${needLayoutItems.size()}")
         onLayout(recycler, needLayoutItems)
-
-        //recycleChildren(recycler)
+        recycleChildren(recycler)
     }
 
     private fun onLayout(recycler: RecyclerView.Recycler?, needLayoutItems: SparseArray<View>) {
@@ -101,7 +101,6 @@ class WalletCartLayoutManager(private val context: Context?) : RecyclerView.Layo
             // 添加 view
             val position = needLayoutItems.keyAt(i)
             val view = recycler!!.getViewForPosition(position)
-            Log.e("onlayout", position.toString())
             addView(view)
             // measure view
             measureChildWithMargins(view, 0, 0)
@@ -122,13 +121,19 @@ class WalletCartLayoutManager(private val context: Context?) : RecyclerView.Layo
 
     private fun getNeedLayoutItems(recycler: RecyclerView.Recycler?): SparseArray<View>{
         val needListViews = SparseArray<View>()
-        val needLayoutItems = findClosestPosition()
-        for(position in needLayoutItems - 2 .. needLayoutItems + 2) {
-            if (position < 0) continue
-            val childAt = recycler!!.getViewForPosition(position)
-            if(childAt != null) {
-                needListViews.put(position, childAt)
+        var currentDistance = 0
+        for(position in 0 until itemCount) {
+            currentDistance = position * childViewWidth + startWidthSpace * 4 + mXoffset
+            if(currentDistance >= 0) {
+                mFirstVisibleItemPos = position
+                break
             }
+        }
+        var endIndex = mFirstVisibleItemPos + 2
+        if(endIndex > itemCount) endIndex = itemCount
+        for(position in mFirstVisibleItemPos .. endIndex) {
+            if(position > itemCount) break
+            needListViews.put(position, recycler!!.getViewForPosition(position))
         }
         return needListViews
     }
@@ -172,10 +177,10 @@ class WalletCartLayoutManager(private val context: Context?) : RecyclerView.Layo
     }
 
     private fun smoothScrollToPosition(position: Int) {
-        Log.e(TAG, "SmoothScroll: $position")
         if (position > -1 && position < itemCount) {
-            if(getChildAt(position) != null) {
-                val dx = getChildAt(position).left - startWidthSpace
+            val viewForPosition = mRecycler!!.getViewForPosition(position)
+            if(viewForPosition != null) {
+                val dx = viewForPosition.left - startWidthSpace
                 val prePosition = mXoffset
                 val targetPosition = mXoffset - dx
                 val animator = ValueAnimator.ofInt(prePosition, targetPosition)
@@ -209,31 +214,14 @@ class WalletCartLayoutManager(private val context: Context?) : RecyclerView.Layo
 
     private fun recycleChildren(recycler: RecyclerView.Recycler?) {
         val scrapList = recycler!!.scrapList
-        for (i in scrapList.indices - 1) {
+        for (i in 0 until scrapList.size - 2) {
             val holder = scrapList[i]
             removeView(holder.itemView)
             recycler.recycleView(holder.itemView)
         }
     }
 
-    private fun recycleChildren(recycler: RecyclerView.Recycler?, startIndex: Int, endIndex: Int) {
-        if(startIndex == endIndex) return
-        if(endIndex > startIndex) {
-            for(position in startIndex .. endIndex) {
-                if(getChildAt(position) != null){
-                    removeAndRecycleViewAt(position, recycler)
-                }else{
-                    break
-                }
-            }
-        }else {
-            for(position in endIndex .. startIndex) {
-                if(getChildAt(position) != null){
-                    removeAndRecycleViewAt(position, recycler)
-                }else{
-                    break
-                }
-            }
-        }
+    companion object {
+        const val TAG = "WalletCartLayoutManager"
     }
 }
--- src/app/src/main/java/com/feng/digitacoin/ui/me/adapter/WalletRecyclerViewAdapter.kt
+++ src/app/src/main/java/com/feng/digitacoin/ui/me/adapter/WalletRecyclerViewAdapter.kt
@@ -1,6 +1,7 @@
 package com.feng.digitacoin.ui.me.adapter
 
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,9 +16,10 @@ import kotlinx.android.synthetic.main.item_wallet_card.view.*
  * @copyright (c) 2018 Newton Foundation. All rights reserved.
  */
 class WalletRecyclerViewAdapter(private val dataSet: List<String>) : RecyclerView.Adapter<WalletRecyclerViewAdapter.ViewHolder>() {
-
+    var index = 0
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_wallet_card, parent, false)
+        index ++
         return ViewHolder(view)
     }
 
@@ -27,11 +29,19 @@ class WalletRecyclerViewAdapter(private val dataSet: List<String>) : RecyclerVie
         holder.bind(dataSet[position])
     }
 
+    override fun getItemViewType(position: Int): Int {
+        return 1
+    }
+
 
     class ViewHolder(private val v: View) : RecyclerView.ViewHolder(v) {
 
         fun bind(string: String) {
             v.text.text = string
         }
+    }
+
+    companion object {
+        const val TAG = "RecyclerViewAdapter"
     }
 }
